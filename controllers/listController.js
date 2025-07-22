90e730887816082d208ca0f119d5a7b61712ad0d
unknown <trongdatga@gmail.com>
2025-07-22 16:42:03 +0700

    edit update

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const List = require("../models/list");
 
 // Lấy tất cả todo items
@@ -24,22 +25,52 @@ exports.createTodo = async (req, res) => {
 };
 
 // Sửa một todo item
+// exports.updateTodo = async (req, res) => {
+//   const { id } = req.params;
+//   const { title, isDone } = req.body;
+
+//   try {
+//     const todo = await List.findById(id);
+//     if (!todo) return res.status(404).send("Not found");
+
+//     todo.title = title;
+//     todo.isDone = isDone;
+//     await todo.save();
+
+//     res.json(todo);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// };
 exports.updateTodo = async (req, res) => {
   const { id } = req.params;
-  const { title, isDone } = req.body;
+
+  // Kiểm tra ObjectId hợp lệ
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid ID format" });
+  }
 
   try {
     const todo = await List.findByIdAndUpdate(
       id,
-      { title, isDone },
-      { new: true }
+      { $set: req.body }, // Sử dụng $set để chỉ cập nhật các trường được gửi lên
+      {
+        new: true, // Trả về document sau khi update
+        runValidators: true, // Kích hoạt validation
+      }
     );
+
     if (!todo) {
-      return res.status(404).send("Todo not found");
+      return res.status(404).json({ error: "Todo not found" });
     }
+
     res.json(todo);
   } catch (error) {
-    res.status(400).send(error);
+    // Trả về lỗi chi tiết từ Mongoose
+    res.status(400).json({
+      error: error.message,
+      details: error.errors,
+    });
   }
 };
 
